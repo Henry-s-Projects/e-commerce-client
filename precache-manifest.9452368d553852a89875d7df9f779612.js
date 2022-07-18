df47e73d62d26856ba04b8c4741adb1780eb7410
truongvukt2000 <truongvukt2000@gmail.com>
2022-07-18 12:00:39 +0700

    Updates

---
+++ precache-manifest.9452368d553852a89875d7df9f779612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "007ef24a95834c96db31",
+    "url": "/Henry-s-Projects/e-commerce-client/static/css/main.1336043c.chunk.css"
+  },
+  {
+    "revision": "007ef24a95834c96db31",
+    "url": "/Henry-s-Projects/e-commerce-client/static/js/main.4a2200b8.chunk.js"
+  },
+  {
+    "revision": "0694f22153d5d525c990",
+    "url": "/Henry-s-Projects/e-commerce-client/static/js/runtime~main.318ba4fe.js"
+  },
+  {
+    "revision": "8f3e87dae138463c5608",
+    "url": "/Henry-s-Projects/e-commerce-client/static/css/2.e89ca997.chunk.css"
+  },
+  {
+    "revision": "8f3e87dae138463c5608",
+    "url": "/Henry-s-Projects/e-commerce-client/static/js/2.79e08384.chunk.js"
+  },
+  {
+    "revision": "8b9f2f2de95a5e82d4bf6a45b784bed7",
+    "url": "/Henry-s-Projects/e-commerce-client/index.html"
+  }
+];
